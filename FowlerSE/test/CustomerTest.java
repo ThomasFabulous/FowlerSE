6bd956fd997fc499dba8d4f3a16044626015a170
hartmann <hartmann@Z3LT119.all.baw.de>
2016-05-02 12:08:10 +0200

    First Commit

---
+++ FowlerSE/test/CustomerTest.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author hartmann
+ */
+public class CustomerTest {
+    
+    public CustomerTest() {
+    }
+     @Test
+    public void statement() throws Exception {
+        String actual;
+        Movie m1 = new Movie("movie1", 1);
+        Movie m2 = new Movie("movie2", 2);
+        Rental r1 = new Rental(m1, 10);
+        Rental r2 = new Rental(m2, 5);
+        Customer c1 = new Customer("joe");
+        c1.addRental(r1);   c1.addRental(r2);
+        actual = c1.statement();
+
+        String expected =
+                "Rental Record for joe\n" +
+                "\tTitle\t\tDays\tAmount\n" +
+                "\tmovie1\t\t10\t30.0\n" +
+                "\tmovie2\t\t5\t4.5\n" +
+                "Amount owed is 34.5\n" +
+                "You earned 3 frequent renter points";
+
+        assertEquals(expected, actual);
+    }
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
